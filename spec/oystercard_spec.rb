dffbebf647ecdee8a647a24a643f78ba60015688
Mrtly <mrtly.ca@gmail.com>
2020-01-27 15:40:25 +0000

    added top up method, updated readme

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,5 +4,15 @@ require 'spec_helper'
 describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
-  end  
+  end 
+  
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument}
+  
+    it 'can top up the balance' do
+        expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+end
+  
 end
